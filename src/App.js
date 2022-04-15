c4c56e1ff9faf4c682573f52f23271addb39c804
natalie <nataliemae263@gmail.com>
2022-04-15 18:37:09 -0500

    unk

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav'
 
 
 
 function App() {
   return (
     <div className="App">
+      <h1>Home</h1>
+      <Nav activeKey="/home">
+    <Nav.Item>
+      <Nav.Link href="/home">Home</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="/about">About Us</Nav.Link>
+    </Nav.Item>
+</Nav>
+
+
+      <Button variant="info">Hello Neverland!</Button>{' '}
       <h1>Bootstrap Website</h1>
-     
+      
     </div>
   );
 }
